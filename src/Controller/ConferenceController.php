96cc52f2158d642856b214a874231e91c34645f1
Aliaksandr Kulba <tunet000@gmail.com>
2022-08-22 14:35:00 +0400

    test deploy

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -11,6 +11,7 @@ class ConferenceController extends AbstractController
     #[Route('/', name: 'homepage')]
     public function index(): Response
     {
+
         return new Response(<<<EOF
         <html>
             <body>
